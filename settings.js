b4005ba2b26c0cc6a9a6faf6fe78733ce141700e
tester1beta <157989143+tester1beta@users.noreply.github.com>
2024-02-01 14:05:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,8 +17,8 @@ global.ONLY_GROUPS = false // Only Groups Use
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OWNER_NAME = 'SAVIYA-MD' // Owner Name
-global.OWNER_NUMBER = ['94768268328,94722617699'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
+global.OWNER_NUMBER = ['94768268328'] //Owner Number
+global.BOT_ADMINS = ['94722617699','94715166712'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
 global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
